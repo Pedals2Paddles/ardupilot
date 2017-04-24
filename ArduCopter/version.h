7a78f2d8712807232add7c06e211329339bc3444
Pedals2Paddles <lawrence.matt@gmail.com>
2017-04-24 15:58:22 -0400

    Copter: Version info for solomod-notify branch
    APM:Copter Solo LED Test 2017-04-24

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -2,11 +2,5 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "APM:Copter V3.6-dev"
+#define THISFIRMWARE "APM:Copter Solo LED Test 2017-04-24"
 #define FIRMWARE_VERSION 3,6,0,FIRMWARE_VERSION_TYPE_DEV
-
-#ifndef GIT_VERSION
-#define FIRMWARE_STRING THISFIRMWARE
-#else
-#define FIRMWARE_STRING THISFIRMWARE " (" GIT_VERSION ")"
-#endif
