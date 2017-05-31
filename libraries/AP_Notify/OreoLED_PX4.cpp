a07d87a1c4cdd027175d2faab5e9298f13b78af8
Hugh Eaves <hugh@hugheaves.com>
2017-05-30 20:10:11 -0400

    De-inline OreoLED_PX4 oreo_state functions.
    Adjust flash rate and phase offset constants for correct byte ordering.

--- libraries/AP_Notify/OreoLED_PX4.cpp
+++ libraries/AP_Notify/OreoLED_PX4.cpp
@@ -35,10 +35,10 @@
 #define OREOLED_BACKRIGHT               1       // back right led instance number
 #define OREOLED_FRONTRIGHT              2       // front right led instance number
 #define OREOLED_FRONTLEFT               3       // front left led instance number
-#define PERIOD_SLOW                     8195    // Slow flash rate
-#define PERIOD_FAST                     62465   // Fast flash rate
-#define PERIOD_SUPER                    38400   // Super fast rate
-#define PO_ALTERNATE                    46080   // 180 degree Phase Offset
+#define PERIOD_SLOW                     800     // Slow flash rate
+#define PERIOD_FAST                     500     // Fast flash rate
+#define PERIOD_SUPER                    150     // Super fast rate
+#define PO_ALTERNATE                    180     // 180 degree Phase Offset
 
 extern const AP_HAL::HAL& hal;
 
@@ -676,4 +676,67 @@ void OreoLED_PX4::handle_led_control(mavlink_message_t *msg)
     }
     _pattern_override = packet.pattern;
 }
+
+OreoLED_PX4::oreo_state::oreo_state() {
+    clear_state();
+}
+
+void OreoLED_PX4::oreo_state::clear_state() {
+    mode = OREOLED_MODE_NONE;
+    pattern = OREOLED_PATTERN_OFF;
+    macro = OREOLED_PARAM_MACRO_RESET;
+    red = 0;
+    green = 0;
+    blue = 0;
+    amplitude_red = 0;
+    amplitude_green = 0;
+    amplitude_blue = 0;
+    period = 0;
+    repeat = 0;
+    phase_offset = 0;
+}
+
+void OreoLED_PX4::oreo_state::send_sync() {
+    clear_state();
+    mode = OREOLED_MODE_SYNC;
+}
+
+void OreoLED_PX4::oreo_state::set_macro(oreoled_macro new_macro) {
+    clear_state();
+    mode = OREOLED_MODE_MACRO;
+    macro = new_macro;
+}
+
+void OreoLED_PX4::oreo_state::set_rgb(enum oreoled_pattern new_pattern, uint8_t new_red, uint8_t new_green, uint8_t new_blue) {
+    clear_state();
+    mode = OREOLED_MODE_RGB;
+    pattern = new_pattern;
+    red = new_red;
+    green = new_green;
+    blue = new_blue;
+}
+
+void OreoLED_PX4::oreo_state::set_rgb(enum oreoled_pattern new_pattern, uint8_t new_red, uint8_t new_green,
+        uint8_t new_blue, uint8_t new_amplitude_red, uint8_t new_amplitude_green, uint8_t new_amplitude_blue,
+        uint16_t new_period, uint16_t new_phase_offset) {
+    clear_state();
+    mode = OREOLED_MODE_RGB_EXTENDED;
+    pattern = new_pattern;
+    red = new_red;
+    green = new_green;
+    blue = new_blue;
+    amplitude_red = new_amplitude_red;
+    amplitude_green = new_amplitude_green;
+    amplitude_blue = new_amplitude_blue;
+    period = new_period;
+    phase_offset = new_phase_offset;
+}
+
+bool OreoLED_PX4::oreo_state::operator==(const OreoLED_PX4::oreo_state &os) {
+   return ((os.mode==mode) && (os.pattern==pattern) && (os.macro==macro) && (os.red==red) && (os.green==green) && (os.blue==blue)
+           && (os.amplitude_red==amplitude_red) && (os.amplitude_green==amplitude_green) && (os.amplitude_blue==amplitude_blue)
+           && (os.period==period) && (os.repeat==repeat) && (os.phase_offset==phase_offset));
+}
+
 #endif // CONFIG_HAL_BOARD == HAL_BOARD_PX4
+
